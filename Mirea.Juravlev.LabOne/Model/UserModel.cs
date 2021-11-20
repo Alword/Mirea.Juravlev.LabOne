5696acc7160dd527a22ea0c61c9432791c1f9d43
ASlepushko <ASlepushko@croc.ru>
2021-11-20 23:01:28 +0300

    Feature user registration page

---
+++ Mirea.Juravlev.LabOne/Model/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Mirea.Juravlev.LabOne
+{
+	public class UserModel
+	{
+		public const int RequiredPasswordLength = 6;
+		public static readonly Regex Simbols = new Regex(@"[A-z]");
+		public static readonly string SimbolsInfo = "только латинские символы";
+
+		/// <summary>ID или имя пользователя</summary>
+		public string Id { get; set; } = string.Empty;
+		/// <summary>Пароль</summary>
+		public string Password { get; set; } = string.Empty;
+		/// <summary>ФИО</summary>
+		public string Represenative { get; set; } = string.Empty;
+		/// <summary>Дата рождения</summary>
+		public DateOnly Birthdate { get; set; }
+		/// <summary>Место рождения</summary>
+		public string Place { get; set; } = string.Empty;
+		/// <summary>Номер телефона</summary>
+		public string PhoneNumber { get; set; } = string.Empty;
+	}
+}
---
+++ Mirea.Juravlev.LabOne/Model/UserStorage.cs
@@ -0,0 +1,14 @@
+﻿namespace Mirea.Juravlev.LabOne
+{
+	public class UserStorage
+	{
+		public UserModel GetUser(string id)
+		{
+			throw new NotImplementedException();
+		}
+		public void SaveUser(UserModel id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
